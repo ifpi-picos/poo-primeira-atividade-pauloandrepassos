e4a953e607560192c265c1411d0d76678811ca7f
Paulo Cesar <pauloreispassos988@gmail.com>
2022-09-16 14:55:38 -0300

    16/09

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import br.edu.ifpi.dominio.Aluno;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(" ");
+        Aluno a1 = new Aluno("Paulo", "paulo@email", "curso123");
+        Aluno a2 = new Aluno("João", "joao@email", "curso795");
+
+        System.out.println("Dados aluno 1: ");
+        a1.status();
+        System.out.println("Dados aluno 2: ");
+        a2.status();
     }
 }
--- src/br/edu/ifpi/dominio/Aluno.java
+++ src/br/edu/ifpi/dominio/Aluno.java
@@ -6,27 +6,42 @@ public class Aluno {
     private String matricula;
 
     public Aluno(String nome, String email, String matricula) {
-        this.nome = nome;
-        this.email = email;
-        this.matricula = matricula;
+        setNome(nome);
+        setEmail(email);
+        setMatricula(matricula);
     }
 
     public String getNome() {
         return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getMatricula() {
         return matricula;
     }
 
-    @Override
-    public String toString() {
-        return "Aluno [email=" + email + ", matricula=" + matricula + ", nome=" + nome + "]";
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
     }
+
+    public void status() {
+        System.out.println("Nome:      " + this.getNome());
+        System.out.println("Email:     " + this.getEmail());
+        System.out.println("Matrícula: " + this.getMatricula() + "\n");
+    }
+
+    
 
     
     
